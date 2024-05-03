72eeac5de600a4e2f118c3ab6035bb7114f01aaa
TRINHDINHDAI2306 <trinhdinhdai2002@gmail.com>
2024-05-03 23:32:07 +0700

    rememberLogin

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,21 @@
-// const dotenv = require("dotenv")
-// module.exports = {
-//     google : {
-//         clientID : process.env.GOOGLE_CLIENT_ID ,
-//         clientSecret : process.env.GOOGLE_CLIENT_SECRET ,
-//         callbackURL : process.env.GOOGLE_CALLBACK_URL 
-//     },
-//     facebook : {
-//         clientID : process.env.FACEBOOK_CLIENT_ID ,
-//         clientSecret : process.env.FACEBOOK_CLIENT_SECRET ,
-//         callbackURL : process.env.FACEBOOK_CALLBACK_URL 
-//     },
-//     facebook : {
-//         clientID : process.env.GITHUB_CLIENT_ID ,
-//         clientSecret : process.env.GITHUB_CLIENT_SECRET ,
-//         callbackURL : process.env.GITHUB_CALLBACK_URL 
-//     },
+const dotenv = require("dotenv")
+module.exports = {
+    google : {
+        clientID : process.env.GOOGLE_CLIENT_ID ,
+        clientSecret : process.env.GOOGLE_CLIENT_SECRET ,
+        callbackURL : process.env.GOOGLE_CALLBACK_URL 
+    },
+    facebook : {
+        clientID : process.env.FACEBOOK_CLIENT_ID ,
+        clientSecret : process.env.FACEBOOK_CLIENT_SECRET ,
+        callbackURL : process.env.FACEBOOK_CALLBACK_URL 
+    },
+    facebook : {
+        clientID : process.env.GITHUB_CLIENT_ID ,
+        clientSecret : process.env.GITHUB_CLIENT_SECRET ,
+        callbackURL : process.env.GITHUB_CALLBACK_URL 
+    },
 
 
 
-// }
+}
